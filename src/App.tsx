64b1f3b87c74e3bdb675c625de6e585bcb27ad7a
litsters <snlitster@gmail.com>
2023-10-05 22:34:03 -0600

    added a manual health check to verify connection between front and back end

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,36 @@
-import { useState } from 'react'
+import { useCallback, useMemo, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import axios from 'axios'
+
+type HealthCheckStatus = "unknown" | "healthy" | "unhealthy";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [healthCheckStatus, setHealthCheckStatus] = useState<HealthCheckStatus>("unknown")
+
+  const doHealthCheck = useCallback(async () => {
+    try{
+      const res = await axios.get("https://saltcontools.hopto.org/api/health");
+      console.log(JSON.stringify(res,null,2))
+      if(res.status === 200 && res.data === "Healthy"){
+        return true;
+      } else {
+        return false;
+      }
+    }catch(e){
+      console.error(e);
+      return false;
+    }
+  },[])
+
+  const healthCheckMessage = useMemo(()=>{
+    switch(healthCheckStatus){
+      case "healthy": return "backend is healthy";
+      case "unhealthy": return "backend is not healthy";
+      case "unknown": return "click to do health check"
+    }
+  },[healthCheckStatus])
 
   return (
     <>
@@ -18,8 +44,8 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={() => doHealthCheck().then(healthy => setHealthCheckStatus(healthy ? "healthy" : "unhealthy"))}>
+          {healthCheckMessage}
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
